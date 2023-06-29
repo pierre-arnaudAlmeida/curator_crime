aaf65584b63cc37a618a45b11425561f65a40489
Pierre-Arnaud <pierre-arnaud94@hotmail.fr>
2023-06-30 00:17:58 +0200

    Add .env.test file, create a message_utils class and compute Embed Message description with parameters

--- commands/warn/warn.js
+++ commands/warn/warn.js
@@ -1,4 +1,33 @@
+require('dotenv').config();
 const { SlashCommandBuilder } = require('discord.js');
+
+function computeDescription(add_or_remove, gang, nbr_of_warns, reason, mission, duration) {
+	let message = null;
+
+	message = "\n\nOs "+ "<@&" + gang.id + "> acabam de ";
+
+	if (add_or_remove) {
+		message += "receber";
+	} else {
+		message += "perder";
+	}
+	
+	message += " um aviso, encontram-se com **WARN ("+ nbr_of_warns + "/"+ process.env.MAX_WARNS +")**";
+	message += "\n\nMotivo : " + reason;
+
+	if (!mission) {
+		message += "\nMissão : " + mission;
+	}
+
+	if (duration != 0) {
+		message += "\nDuração : " + duration + " dias";
+	}
+
+	// Add mention of curator and senior curator defined in environment variables
+	message += "\n\n" + "";
+
+	return message;
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -27,13 +56,21 @@ module.exports = {
 		.addStringOption(option =>
 			option.setName('mission')
 				.setDescription('Specify the mission')
-				.setRequired(true))
+				.setRequired(false))
 		.addIntegerOption(option =>
 			option.setName('duration')
 				.setDescription('Give a duration')
 				.setRequired(false)),
 	async execute(interaction) {
-		const gang = interaction.options.getUser('gang');
+		const add_or_remove = interaction.options.getBoolean('add_or_remove');
+		const gang = interaction.options.getRole('gang');
+		const channel = interaction.options.getChannel('channel');
+		const nbr_of_warns = interaction.options.getInteger('nbr_of_warns');
+		const reason = interaction.options.getString('reason');
+		const mission = interaction.options.getString('mission') ?? null;
+		const duration = interaction.options.getInteger('duration') ?? null;
+
+		await channel.send(computeDescription(add_or_remove, gang, nbr_of_warns, reason, mission, duration));
 		await interaction.reply('Message sent');
 	},
 };
