cf67acdb54618f89adb448c64206e6b92f46e87d
Pierre-Arnaud <pierre-arnaud94@hotmail.fr>
2023-06-30 22:21:27 +0200

    Add turf process

--- commands/turf/turf.js
+++ commands/turf/turf.js
@@ -1,4 +1,26 @@
+require('dotenv').config();
 const { SlashCommandBuilder } = require('discord.js');
+const MessageUtils = require('../../utils/messageUtils');
+
+let reseted = null;
+let gang = null;
+let channel = null;
+let completed = null;
+
+function computeCompletedDescription(gang) {
+	return"\n\nVenho dar os parabéns aos <@&" + gang.id + "> por capturar 100% dos territórios.\n\nObrigado pelo empenho e boa sorte na próxima temporada.";
+}
+
+function computeResetedDescription(gang) {
+	return "\n\nVenho informar aos <@&" + gang.id + "> que os territórios foram reiniciados.\n\nBoa sorte na temporada.";
+}
+
+function getParameters(interaction) {
+	reseted = interaction.options.getBoolean('reseted');
+	gang = interaction.options.getRole('gang');
+	channel = interaction.options.getChannel('channel');
+	completed = interaction.options.getBoolean('completed');
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -21,6 +43,18 @@ module.exports = {
 				.setDescription('100% turf have been completed ? 0 : NO and 1 : YES')
 				.setRequired(true)),
 	async execute(interaction) {
+		getParameters(interaction);
+
+		if (completed) {
+			await MessageUtils.sendEmbed(channel, gang, MessageUtils.createEmbed("Anuncio turfs", computeCompletedDescription(gang), Colors.Red), interaction)
+			return message1;
+		} 
+		
+		if (reseted) {
+			await MessageUtils.sendEmbed(channel, gang, MessageUtils.createEmbed("Anuncio turfs", computeResetedDescription(reseted), Colors.Red), interaction)
+			return message2;
+		}
+
 		await interaction.reply('Message sent');
 	},
 };
