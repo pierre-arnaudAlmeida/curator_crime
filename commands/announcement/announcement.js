b489508c8d4e916c2901059840a0408291c187d6
Pierre-Arnaud <pierre-arnaud94@hotmail.fr>
2023-06-30 22:20:31 +0200

    Add annoucement process

--- commands/announcement/announcement.js
+++ commands/announcement/announcement.js
@@ -1,11 +1,27 @@
-const { SlashCommandBuilder } = require('discord.js');
+require('dotenv').config();
+const { SlashCommandBuilder, Colors } = require('discord.js');
+const MessageUtils = require('../../utils/messageUtils');
+
+let gang = null;
+let channel = null;
+let message = null;
+
+function computeDescription(message) {
+	return "\n\n" + message;
+}
+
+function getParameters(interaction) {
+	gang = interaction.options.getRole('gang');
+	channel = interaction.options.getChannel('channel');
+	message = interaction.options.getString('message');
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('announcement')
 		.setDescription('You can send an announcement')
 		.addRoleOption(option =>
-			option.setName('who')
+			option.setName('gang')
 				.setDescription('Mention gang with @')
 				.setRequired(true))
 		.addChannelOption(option =>
@@ -17,6 +33,9 @@ module.exports = {
 				.setDescription('Specify the message')
 				.setRequired(true)),
 	async execute(interaction) {
+		getParameters(interaction);
+
+		await MessageUtils.sendEmbed(channel, gang, MessageUtils.createEmbed("Aviso", computeDescription(message), Colors.Red), interaction)
 		await interaction.reply('Message sent');
 	},
 };
