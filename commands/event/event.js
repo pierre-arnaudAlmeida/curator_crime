7a62a9d6886724c60e059454be9e7c5374d13e44
Pierre-Arnaud <pierre-arnaud94@hotmail.fr>
2023-06-30 22:21:00 +0200

    Add event process

--- commands/event/event.js
+++ commands/event/event.js
@@ -1,4 +1,37 @@
-const { SlashCommandBuilder } = require('discord.js');
+require('dotenv').config();
+const { SlashCommandBuilder, Colors } = require('discord.js');
+const MessageUtils = require('../../utils/messageUtils');
+const BotUtils = require('../../utils/botUtils');
+
+let gang = null;
+let channel = null;
+let percentage = null;
+let completed = null;
+
+function computeDescription(gang, percentage, completed) {
+	let message = null;
+	const localDate = new Date();
+	const firstDay = BotUtils.subtractDays(localDate, 7);
+    const lastDay = BotUtils.subtractDays(localDate, 1);
+
+	message = "\n\nVenho informar que os "+ "<@&" + gang.id + "> realizaram **" + percentage + "%** de participação em eventos nesta semana ";
+	message += "(" + BotUtils.computeDate(firstDay) + " a " + BotUtils.computeDate(lastDay) + ")";
+
+	if (completed) {
+		message += "\n\nObrigado pelo empenho e boa sorte na próxima semana";
+	} else {
+		message += "Pedimos mais empenho e boa sorte na próxima semana";
+	}
+
+	return message;
+}
+
+function getParameters(interaction) {
+	gang = interaction.options.getRole('gang');
+	channel = interaction.options.getChannel('channel');
+	percentage = interaction.options.getInteger('percentage');
+	completed = interaction.options.getBoolean('completed');
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -21,6 +54,9 @@ module.exports = {
 				.setDescription('Event requirement completed ?')
 				.setRequired(true)),
 	async execute(interaction) {
+		getParameters(interaction);
+
+		await MessageUtils.sendEmbed(channel, gang, MessageUtils.createEmbed("Aviso", computeDescription(gang, percentage, completed), Colors.Red), interaction)
 		await interaction.reply('Message sent');
 	},
 };
