db0c4b64202c7f262d397ecc0fa4aaa59637e7fd
Pierre-Arnaud <pierre-arnaud94@hotmail.fr>
2023-06-30 22:18:13 +0200

    Add message utils

---
+++ utils/messageUtils.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder } = require('discord.js');
+const BotUtils = require('./botUtils');
+
+module.exports = class MessageUtils {
+    
+    static createEmbed (title, description, color) {
+        const embedBuilder = new EmbedBuilder();
+
+        embedBuilder.setColor(color);
+        embedBuilder.setTitle(title);
+        embedBuilder.setDescription(description);
+
+        return embedBuilder;
+    }
+
+    static sendEmbed (channel, gang, embedBuilder, interaction) {
+        const Curator = BotUtils.getCurator(interaction);
+        const SeniorCurator = BotUtils.getSeniorCurator(interaction);
+
+        channel.send({ embeds: [embedBuilder],
+        content: "<@&" + gang.id + ">"});
+        channel.send({ content: `${Curator ? `${Curator}` : "role not found"} \n${SeniorCurator ? `${SeniorCurator}` : "role not found"}`});
+    }
+
+}
